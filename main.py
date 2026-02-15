f36e6c3a60ee2162e63023d8206d0c3dc3dd7b2c
RajGajjar-01 <irajgajjar.2004@gmail.com>
2026-02-15 16:29:26 +0530

    feat: db connection and create user model

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from app.utils.init_db import create_tables
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+# In lifespan function before yield events happen at the start of app and after yield events happen at closing time of the application
+
+@asynccontextmanager #TODO : Learn about this
+async def lifespan(app:FastAPI):
+    print("created")
+    create_tables()
+    yield # Seperation Point
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/health")
 def health_check():
